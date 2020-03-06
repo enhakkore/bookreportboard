9c8544d4288a82e69bca1339edd022ea1a8bdca8
enhakkore <ahn.ephraim@gmail.com>
2020-03-06 20:09:15 +0900

    json 응답값 테스트

--- src/test/java/com/practice/bookreportboard/web/HelloControllerTest.java
+++ src/test/java/com/practice/bookreportboard/web/HelloControllerTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,5 +27,18 @@ class HelloControllerTest {
                 .andExpect(content().string(hello));
 
         System.out.println("TEST 끝");
+    }
+
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        String name = "name";
+        int amount = 10000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is(name)))
+            .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
