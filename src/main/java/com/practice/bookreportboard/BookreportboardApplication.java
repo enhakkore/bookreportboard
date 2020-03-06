88bdcb7607f7ad4b13b0abf5e854036afe741097
enhakkore <ahn.ephraim@gmail.com>
2020-03-06 17:20:47 +0900

    Initial commit

---
+++ src/main/java/com/practice/bookreportboard/BookreportboardApplication.java
@@ -0,0 +1,13 @@
+package com.practice.bookreportboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreportboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookreportboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/bookreportboard/BookreportboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.bookreportboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreportboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
