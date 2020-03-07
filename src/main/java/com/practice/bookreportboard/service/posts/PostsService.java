940192b6dced7df9fa7bc1667bfe38a312e66311
enhakkore <ahn.ephraim@gmail.com>
2020-03-07 12:30:00 +0900

    PostApi 생성 및 테스

---
+++ src/main/java/com/practice/bookreportboard/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.practice.bookreportboard.service.posts;
+
+import com.practice.bookreportboard.domain.posts.PostsRepository;
+import com.practice.bookreportboard.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto postsSaveRequestDto){
+        return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+}
---
+++ src/test/java/com/practice/bookreportboard/web/PostsApiControllerTest.java
@@ -0,0 +1,55 @@
+package com.practice.bookreportboard.web;
+
+import com.practice.bookreportboard.domain.posts.Posts;
+import com.practice.bookreportboard.domain.posts.PostsRepository;
+import com.practice.bookreportboard.web.dto.PostsSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() {
+        String title = "제목";
+        String author = "저자";
+        String content = "테스트내용";
+        PostsSaveRequestDto postsSaveRequestDto = PostsSaveRequestDto.builder().title(title).author(author).content(content).build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, postsSaveRequestDto, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+}
