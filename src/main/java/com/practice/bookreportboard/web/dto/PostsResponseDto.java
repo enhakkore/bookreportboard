51104300e6d1764362eb61da17068c1b09f71324
enhakkore <ahn.ephraim@gmail.com>
2020-03-09 19:57:45 +0900

    PostsResponseDto 추가

---
+++ src/main/java/com/practice/bookreportboard/web/dto/PostsResponseDto.java
@@ -0,0 +1,17 @@
+package com.practice.bookreportboard.web.dto;
+
+import com.practice.bookreportboard.domain.posts.Posts;
+
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String content;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.content = entity.getContent();
+    }
+}
