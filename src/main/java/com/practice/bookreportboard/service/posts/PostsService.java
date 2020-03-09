9de79697861b20e33d63fa0ad5ddd11cccaa71aa
enhakkore <ahn.ephraim@gmail.com>
2020-03-09 20:03:15 +0900

    PostsService에 update, get 메서드 추

--- src/main/java/com/practice/bookreportboard/service/posts/PostsService.java
+++ src/main/java/com/practice/bookreportboard/service/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.practice.bookreportboard.service.posts;
 
+import com.practice.bookreportboard.domain.posts.Posts;
 import com.practice.bookreportboard.domain.posts.PostsRepository;
+import com.practice.bookreportboard.web.dto.PostsResponseDto;
 import com.practice.bookreportboard.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto postsSaveRequestDto){
         return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsSaveRequestDto postsSaveRequestDto){
+        Posts post = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 id가 없습니다. id = " + id));
+        post.update(postsSaveRequestDto.getTitle(), postsSaveRequestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto get(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 id가 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
     }
 }
