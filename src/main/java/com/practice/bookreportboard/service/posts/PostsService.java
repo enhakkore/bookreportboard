e3e37cd1e7e340331d5e9ce777805f877ca7859b
enhakkore <ahn.ephraim@gmail.com>
2020-03-11 20:19:25 +0900

    findAllDesc 메서드 추가

--- src/main/java/com/practice/bookreportboard/service/posts/PostsService.java
+++ src/main/java/com/practice/bookreportboard/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.practice.bookreportboard.service.posts;
 
 import com.practice.bookreportboard.domain.posts.Posts;
 import com.practice.bookreportboard.domain.posts.PostsRepository;
+import com.practice.bookreportboard.web.dto.PostsListResponseDto;
 import com.practice.bookreportboard.web.dto.PostsResponseDto;
 import com.practice.bookreportboard.web.dto.PostsSaveRequestDto;
 import com.practice.bookreportboard.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +34,10 @@ public class PostsService {
     public PostsResponseDto get(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 id가 없습니다. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(toList());
     }
 }
