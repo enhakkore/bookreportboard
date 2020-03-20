59bf0961535df773893f151ad8a421444768f141
enhakkore <ahn.ephraim@gmail.com>
2020-03-20 17:37:20 +0900

    Posts 엔티티에 필드 추가

--- src/main/java/com/practice/bookreportboard/domain/posts/Posts.java
+++ src/main/java/com/practice/bookreportboard/domain/posts/Posts.java
@@ -24,11 +24,20 @@ public class Posts extends BaseTimeEntity {
 
     private String author;
 
+    private String bookTitle;
+    private String bookImage;
+    private String bookPublisher;
+    private String bookAuthor;
+
     @Builder
-    public Posts(String title, String content, String author){
+    public Posts(String title, String content, String author, String bookTitle, String bookAuthor, String bookImage, String bookPublisher){
         this.title = title;
         this.content = content;
         this.author = author;
+        this.bookAuthor = bookAuthor;
+        this.bookImage = bookImage;
+        this.bookPublisher = bookPublisher;
+        this.bookTitle = bookTitle;
     }
 
     public void update(String title, String content){
