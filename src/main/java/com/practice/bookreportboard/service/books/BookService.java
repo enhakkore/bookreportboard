a3f86c5af2141c8c603785d50d81538c436ddabe
enhakkore <ahn.ephraim@gmail.com>
2020-05-18 18:10:52 +0900

    사용하지 않는 코드 정리

--- src/main/java/com/practice/bookreportboard/service/books/BookService.java
+++ src/main/java/com/practice/bookreportboard/service/books/BookService.java
@@ -1,17 +1,11 @@
 package com.practice.bookreportboard.service.books;
 
-import com.practice.bookreportboard.domain.books.Book;
 import com.practice.bookreportboard.domain.books.kakao.KakaoBookWebClient;
-import com.practice.bookreportboard.domain.books.naver.NaverBookRestTemplate;
-import com.practice.bookreportboard.domain.books.kakao.KakaoBookRestTemplate;
-import com.practice.bookreportboard.domain.books.kakao.KakaoBooks;
 import com.practice.bookreportboard.domain.books.naver.NaverBookWebClient;
-import com.practice.bookreportboard.domain.books.naver.NaverBooks;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
@@ -19,9 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,14 +22,6 @@ public class BookService {
 
     private final KakaoBookWebClient kakaoBookWebClient;
     private final NaverBookWebClient naverBookWebClient;
-
-    private final NaverBookRestTemplate naverBookRestTemplate;
-    private final KakaoBookRestTemplate kakaoBookRestTemplate;
-
-    public List<Book> search(String bookTitle){
-        // naver api 오류 응답에 대한 exception 처리를 하지 못함.
-        ResponseEntity<NaverBooks> naverResponse = naverBookSearch(bookTitle);
-        List<Book> results = naverResponse.getBody().getItems().stream().map(item -> new Book(item)).collect(Collectors.toList());
 
     public Mono<ClientResponse> kakaoBookSearch(String title){
         ExchangeFilterFunction errorHandleFilter = KakaoBookWebClient.createResponseFilter( clientResponse -> {
