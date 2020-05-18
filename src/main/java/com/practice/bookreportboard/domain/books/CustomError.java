233d465fedcfeaa712dd7b6891cb965ff52b2732
enhakkore <ahn.ephraim@gmail.com>
2020-05-18 17:17:19 +0900

    응답 에러 처리를 위한 CustomError 클래스 추가

---
+++ src/main/java/com/practice/bookreportboard/domain/books/CustomError.java
@@ -0,0 +1,10 @@
+package com.practice.bookreportboard.domain.books;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomError {
+    private String errorContent;
+}
