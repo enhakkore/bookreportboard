adcc60a4cd6c06c5435e4aad5392a5b024cb6d9c
enhakkore <ahn.ephraim@gmail.com>
2020-05-18 17:16:20 +0900

    NaverBooks 상속 추가

--- src/main/java/com/practice/bookreportboard/domain/books/kakao/KakaoBooks.java
+++ src/main/java/com/practice/bookreportboard/domain/books/kakao/KakaoBooks.java
@@ -1,5 +1,6 @@
 package com.practice.bookreportboard.domain.books.kakao;
 
+import com.practice.bookreportboard.domain.books.naver.NaverBooks;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
-public class KakaoBooks {
+public class KakaoBooks extends NaverBooks {
     private Meta meta;
     private List<Document> documents;
 }
