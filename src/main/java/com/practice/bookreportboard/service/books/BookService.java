aa01c9fdbb15541ac93d9e3351794f010e0090bd
enhakkore <ahn.ephraim@gmail.com>
2020-06-03 14:41:10 +0900

    카카오 API 에러 발생시 네이버 API 사용하도록 수정

--- src/main/java/com/practice/bookreportboard/service/books/BookService.java
+++ src/main/java/com/practice/bookreportboard/service/books/BookService.java
@@ -1,17 +1,15 @@
 package com.practice.bookreportboard.service.books;
 
-import com.practice.bookreportboard.domain.books.Book;
 import com.practice.bookreportboard.domain.books.naver.NaverBookRestTemplate;
 import com.practice.bookreportboard.domain.books.kakao.KakaoBookRestTemplate;
 import com.practice.bookreportboard.domain.books.kakao.KakaoBooks;
-import com.practice.bookreportboard.domain.books.naver.NaverBooks;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BookService {
@@ -19,18 +17,20 @@ public class BookService {
     private final NaverBookRestTemplate naverBookRestTemplate;
     private final KakaoBookRestTemplate kakaoBookRestTemplate;
 
-    public List<Book> search(String bookTitle){
-        // naver api 오류 응답에 대한 exception 처리를 하지 못함.
-        ResponseEntity<NaverBooks> naverResponse = naverBookSearch(bookTitle);
-        List<Book> results = naverResponse.getBody().getItems().stream().map(item -> new Book(item)).collect(Collectors.toList());
+    public ResponseEntity<KakaoBooks> search(String title){
+        naverBookRestTemplate.setMethod("GET")
+                            .setQuery(title);
 
-        ResponseEntity<KakaoBooks> kakaoResponse = kakaoBookSearch(bookTitle);
-        return kakaoResponse.getBody().getDocuments().stream().map(doc -> new Book(doc)).collect(Collectors.toList());
-    }
-    public ResponseEntity<NaverBooks> naverBookSearch(String bookTitle){
-        return naverBookRestTemplate.search(bookTitle);
-    }
-    public ResponseEntity<KakaoBooks> kakaoBookSearch(String bookTitle){
-        return kakaoBookRestTemplate.search(bookTitle);
+        return kakaoBookRestTemplate.setMethod("GET")
+                            .setQuery("title", title)
+                            .addInterceptor((request, body, execution) -> {
+                                ClientHttpResponse response = execution.execute(request, body);
+                                if(response.getRawStatusCode() >= 400) {
+                                    log.error("카카오 API 응답 에러 발생, 에러 코드 : {}", response.getRawStatusCode());
+                                    return execution.execute(naverBookRestTemplate.getHttpRequest(), body);
+                                }
+                                return response;
+                            })
+                            .exchange();
     }
 }
