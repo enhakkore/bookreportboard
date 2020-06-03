45500b1f1259e001ab7490d424eb4311de113d1d
enhakkore <ahn.ephraim@gmail.com>
2020-06-03 21:55:24 +0900

    PostsApiRouterFunction 빈 등록

--- src/main/java/com/practice/bookreportboard/config/CustomConfig.java
+++ src/main/java/com/practice/bookreportboard/config/CustomConfig.java
@@ -1,17 +1,21 @@
 package com.practice.bookreportboard.config;
 
 import com.practice.bookreportboard.web.PageHandler;
+import com.practice.bookreportboard.web.PostsApiHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.*;
 
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
 @RequiredArgsConstructor
 @Configuration
 public class CustomConfig {
 
     private final PageHandler pageHandler;
+    private final PostsApiHandler postsApiHandler;
 
     @Bean
     public RouterFunction<ServerResponse> pageRouterFunction() {
@@ -20,5 +24,15 @@ public class CustomConfig {
                 .andRoute(GET("/posts/update/{id}"), pageHandler::postsUpdate)
                 .andRoute(GET("/search"), pageHandler::search)
                 .andRoute(GET("/search/results/{bookTitle}"), pageHandler::bookSearchResults);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> postsApiRouterFunction() {
+        return RouterFunctions.nest(path("/api/v1/posts"),
+                RouterFunctions.route(POST(""), postsApiHandler::save)
+                    .andNest(path("/{id}"),
+                                RouterFunctions.route(PATCH(""), postsApiHandler::update)
+                                    .andRoute(GET(""), postsApiHandler::get)
+                                    .andRoute(DELETE(""), postsApiHandler::delete)));
     }
 }
