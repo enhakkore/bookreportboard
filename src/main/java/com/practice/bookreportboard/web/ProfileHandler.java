29cc6df684071463dc43cc836dd2c448d29cf611
enhakkore <ahn.ephraim@gmail.com>
2020-06-08 22:15:49 +0900

    ProfileHandler 추가

---
+++ src/main/java/com/practice/bookreportboard/web/ProfileHandler.java
@@ -0,0 +1,30 @@
+package com.practice.bookreportboard.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class ProfileHandler {
+
+    private final Environment environment;
+
+    public Mono<ServerResponse> profile(ServerRequest serverRequest) {
+        List<String> profiles = Arrays.asList(environment.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        String bodyValue = profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+
+        return ServerResponse.ok().body(BodyInserters.fromValue(bodyValue));
+    }
+
+}
